857788e1ebb0fb7aff10b6b6b7bc69c3c03a63d2
Pouyan <pouyan.zarbafian@hotmail.com>
2022-12-25 16:47:22 -0500

    fix img url

--- frontend/src/TopBar.js
+++ frontend/src/TopBar.js
@@ -1,30 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class TopBar extends Component {
-
-    render() {
-         return (
-        <div className="topbar">
-            <form className="topbar__searchBox">
-                <input
-                    className="topbar__searchBox--input"
-                    type="text"
-                    id="inputSearchBox"
-                    name="Search"
-                    placeholder="Search" 
-                />
-                <img src="../Assets/Icons/Search.svg" className="topbar__searchBox--img"
-                alt='myImg'/>
-            </form>
-            <div className="topbar__rightBox">
-                <img src="../Assets/Avatar/Avatar.jpg" className="topbar__rightBox--userImg"
-                alt='myImg'/>
-                <div className="topbar__rightBox--optionForm">
-                    <img src="../Assets/Icons/Dropdown.svg" className="topbar__rightBox--optionForm__icon"
-                    alt='myImg'/>
-                </div>
-            </div>
+  render() {
+    return (
+      <div className="topbar">
+        <form className="topbar__searchBox">
+          <input
+            className="topbar__searchBox--input"
+            type="text"
+            id="inputSearchBox"
+            name="Search"
+            placeholder="Search"
+          />
+          <img
+            src="/Assets/Icons/Search.svg"
+            className="topbar__searchBox--img"
+            alt="myImg"
+          />
+        </form>
+        <div className="topbar__rightBox">
+          <img
+            src="../Assets/Avatar/Avatar.jpg"
+            className="topbar__rightBox--userImg"
+            alt="myImg"
+          />
+          <div className="topbar__rightBox--optionForm">
+            <img
+              src="../Assets/Icons/Dropdown.svg"
+              className="topbar__rightBox--optionForm__icon"
+              alt="myImg"
+            />
+          </div>
         </div>
-         )
-    }
+      </div>
+    );
+  }
 }
